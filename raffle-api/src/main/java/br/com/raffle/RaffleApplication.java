057880fa9d0eaafa5fab86c42e74b05456314bde
rafael moura <rafaelfelipe.peixoto@gmail.com>
2024-12-15 20:21:29 -0300

    initiating project

---
+++ raffle-api/src/main/java/br/com/raffle/RaffleApplication.java
@@ -0,0 +1,13 @@
+package br.com.raffle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaffleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaffleApplication.class, args);
+	}
+
+}
